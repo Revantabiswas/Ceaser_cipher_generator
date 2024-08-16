3df5da3b986c89ca3fa614e384ff399396abc5a8
Revanta Biswas <122297743+Revantabiswas@users.noreply.github.com>
2024-08-16 12:24:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,34 +2,32 @@ from logo import logo
 print(logo)
 import string
 alphabets = list(string.ascii_lowercase)
-direction=input("Type'encode' to encrypt,Type 'decode' to decrypt\n")
+def get_direction():
+    while True:
+        direction = input("Type 'encode' to encrypt, Type 'decode' to decrypt\n").lower()
+        if direction in ["encode", "decode"]:
+            return direction
+        else:
+            print("Invalid input. Please type 'encode' or 'decode'.")
+
+direction = get_direction()
 text=input("Enter the text\n").lower()
 shifts=int(input("Enter the amount of shifts you want\n"))
 
-
-
-def encrypt(plain_text,n_shift):
-  
-  encrypted_text = ""
-  for letter in plain_text:
-    position=alphabets.index(letter)
-    new_position=position + n_shift
-    new_letter=alphabets[new_position]
-    encrypted_text += new_letter
-  print(f'The encoded text is {encrypted_text}')
-
-def decrypt(cipher_text,nshift):
-  
-  decrypted_text = ""
-  for letter in cipher_text:
-    position=alphabets.index(letter)
-    new_position = position - nshift
-    decrypted_text += alphabets[new_position]
-  print(f"The decoded text is {decrypted_text}")
-#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
-if direction=="encode":
-   encrypt(plain_text=text,n_shift=shifts)
-elif direction=="decode":
-  decrypt(cipher_text=text,nshift=shifts)
+def ceaser(original_text,n_shift,directions):
+  output_text=""
+  for letter in original_text:
+    if letter in alphabets:
+      position=alphabets.index(letter)
+      if directions=="encode":
+        new_position=position+n_shift
+        new_letter=alphabets[new_position]
+        output_text+=new_letter
+      elif directions=="decode":
+        new_position=position-n_shift
+        new_letter=alphabets[new_position]
+        output_text+=new_letter
+  print(f"The {direction} text is {output_text}")
+ceaser(original_text=text,n_shift=shifts,directions=direction)
 
 
